ea8681b93b248b4c364dd787f202ffeb933bfca2
aminjafarlou <jafarlou.amin@gmail.com>
2024-01-10 15:42:48 +0330

    add middlewares for checking ID and body

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,32 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, value) => {
+  const id = value * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      result: "fail",
+      requestedAt: req.requestTime,
+      message: "Invalid ID",
+    });
+  }
+
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "failed",
+      requestedAt: req.requestTime,
+      message: "Missing name or price",
+    });
+  }
+
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
@@ -16,16 +42,8 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      result: "fail",
-      requestedAt: req.requestTime,
-      message: "Invalid ID",
-    });
-  }
-
   const tour = tours.find((item) => item.id === id);
+
   res.status(200).json({
     status: "success",
     requestedAt: req.requestTime,
@@ -55,16 +73,6 @@ exports.createNewTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      result: "fail",
-      requestedAt: req.requestTime,
-      message: "Invalid ID",
-    });
-  }
-
   res.status(200).json({
     status: "success",
     requestedAt: req.requestTime,
@@ -75,16 +83,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      result: "fail",
-      requestedAt: req.requestTime,
-      message: "Invalid ID",
-    });
-  }
-
   res.status(204).json({
     status: "success",
     requestedAt: req.requestTime,
